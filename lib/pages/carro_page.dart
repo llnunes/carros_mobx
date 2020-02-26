e2a072b9210e4b6404c3d6ba184b7cf98582fcc8
lucas <lucas.lopesdf@gmail.com>
2020-02-25 22:01:46 -0300

    Padrao MobX

--- lib/pages/carro_page.dart
+++ lib/pages/carro_page.dart
@@ -1,4 +1,5 @@
 import 'package:carros/model/carro.dart';
+import 'package:carros/util/text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,35 @@ class CarroPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(carro.nome),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.place),
+            onPressed: _onClickMap,
+          ),
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: _onClickVideo,
+          ),
+          PopupMenuButton<String>(
+            onSelected: _onClickPopupMenu,
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Share",
+                  child: Text("Share"),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: _body(),
     );
@@ -20,7 +50,84 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
       padding: EdgeInsets.all(16),
-      child: Image.network(carro.urlFoto),
+      child: ListView(
+        children: <Widget>[
+          Image.network(carro.urlFoto),
+          _bloco1(),
+          Divider(),
+          _bloco2()
+        ],
+      ),
     );
   }
+
+  Row _bloco1() {
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                text(carro.nome, fontSize: 22, bold: true),
+                text(carro.tipo, fontSize: 16, bold: false),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                    size: 40,
+                  ),
+                  onPressed: _onClickFavorito,
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.share,
+                    size: 40,
+                  ),
+                  onPressed: _onClickShare,
+                )
+              ],
+            )
+          ],
+        );
+  }
+
+  _bloco2() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 20,),
+        text(carro.descricao, fontSize: 16, bold: true),
+        SizedBox(height: 20,),
+        text("Texto grande ", fontSize: 16),
+      ],
+    );
+  }
+
+  void _onClickMap() {}
+
+  void _onClickVideo() {}
+
+  _onClickPopupMenu(String value) {
+    switch (value) {
+      case "Editar":
+        print("editar");
+        break;
+      case "Deletar":
+        print("deletar");
+        break;
+      case "Share":
+        print("share");
+        break;
+    }
+  }
+
+  void _onClickShare() {}
+
+  void _onClickFavorito() {}
+
+
 }
